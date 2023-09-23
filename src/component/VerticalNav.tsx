152466ab5d27b6fc1109f65de1f2f1c300d58a5f
Gary T Almes <garytalmes@gmail.com>
2023-09-23 05:23:47 -0400

    Prepping for new branch.

--- src/component/VerticalNav.tsx
+++ src/component/VerticalNav.tsx
@@ -1,3 +1,5 @@
+import "./VerticalNav.css"
+
 export interface INavItem {
   id: string | number
   label: string
@@ -5,19 +7,51 @@ export interface INavItem {
   href?: string
 }
 
-export interface IVerticalNav {
-  items: INavItem[]
+export interface INavConfig {
+  useFlex?: boolean | undefined
+  list?: {
+    id?: string | undefined
+    classes?: string | undefined
+  }
+  items?: {
+    classes?: string | undefined
+  }
+  icons?: {
+    classes?: string | undefined
+  }
 }
 
-export const VerticalNav = ({ items }: IVerticalNav) => {
+export interface IVerticalNav {
+  items: INavItem[]
+  config?: INavConfig
+}
+
+const baseConfig: any = {
+  useFlex: true,
+  list: {
+    id: "vertical-nav-list",
+  },
+  items: {
+    classes: "vertical-nav-item flex",
+  },
+  icons: {
+    classes: "vertical-nav-icon",
+  },
+}
+
+export const VerticalNav = ({ items, config = {}, ...rest }: IVerticalNav) => {
+  const finalConfig = { ...baseConfig, ...config }
   return (
-    <div style={{ width: "100%", position: "relative" }}>
-      <ul style={{ listStyleType: "none", margin: 0, padding: 0 }}>
+    <div {...rest}>
+      <ul id={finalConfig?.list?.id}>
         {items.map((item: INavItem) => (
-          <li
-            key={item.id}
-            style={{ padding: "1px 3px", fontSize: ".95rem", backgroundColor: "transparent" }}
-          >
+          <li key={item.id} className={finalConfig?.items?.classes}>
+            {item.svg !== undefined && (
+              <img
+                className={finalConfig?.icons?.classes}
+                src={`data:image/svg+xml;utf8,${encodeURIComponent(item.svg)}`}
+              />
+            )}
             <a href={item.href || "#"} style={{ textDecoration: "none" }}>
               {item.label}
             </a>
--- src/component/index.ts
+++ src/component/index.ts
@@ -1,2 +1,3 @@
-export type { INavItem, IVerticalNav } from "./VerticalNav"
+export type { INavConfig, INavItem, IVerticalNav } from "./VerticalNav"
 export { VerticalNav } from "./VerticalNav"
+export * from "./VerticalNav.css"
